e2bb57d9d7b7a104f1a7926ba02fc526a07fc949
Oksana Svynarova <oksankasvn@gmail.com>
2023-05-07 18:37:28 +0300

    feat: implement 04-copy-directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,30 @@
+const fs = require('fs');
+const path = require('path');
 
+const folder = path.join(__dirname, 'files');
+const folderCopy = path.join(__dirname, 'files-copy');
+
+fs.rm(folderCopy, {
+  recursive: true, force: true
+}, (err) => {
+  if (err) throw err;
+  fs.readdir(folder, {
+    withFileTypes: true
+  }, (err, files) => {
+    if (err) throw err;
+    fs.mkdir(folderCopy, {
+      recursive: true, force: true
+    }, err => {
+      if (err) throw err;
+      console.log('Folder successfully created');
+      files.forEach(file => {
+        fs.copyFile(path.join(__dirname, 'files', file.name),
+          path.join(__dirname, 'files-copy', file.name),
+          (err) => {
+            if (err) throw err;
+          });
+      });
+    });
+  }
+  );
+});
